cc86fd98fe768fc9f3ff311f1c94a2b34bec8ab9
bernice75 <good12388@daum.net>
2020-06-14 12:00:00 +0900

    covid1

--- chart/urls.py
+++ chart/urls.py
@@ -10,6 +10,8 @@ urlpatterns = [
          views.ticket_class_view_2, name='ticket_class_view_2'),
     path('ticket-class/3/',
          views.ticket_class_view_3, name='ticket_class_view_3'),
+path('covid1/',
+         views.covid1, name='covid1'),
     path('world-population/',
          views.world_population, name='world_population'),  # !!!
     path('json-example/', views.json_example, name='json_example'),
